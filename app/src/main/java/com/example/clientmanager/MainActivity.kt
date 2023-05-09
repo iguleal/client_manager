5ea3c2012ce67f6aca9d3d2b69d0c5ef4a9bf039
Igor Leal <igorleal96@gmail.com>
2023-05-09 11:15:46 -0300

    recycle view criada
    recycle view iniciada e pronta pra configuração

--- app/src/main/java/com/example/clientmanager/MainActivity.kt
+++ app/src/main/java/com/example/clientmanager/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.clientmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvMain: RecyclerView = findViewById(R.id.rv_main)
+
+        val adapter = MainAdapter()
+        rvMain.adapter = adapter
+        rvMain.layoutManager = GridLayoutManager(this, 2)
     }
+
+ private inner class MainAdapter(): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
+     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.MainViewHolder {
+         val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+         return MainViewHolder(view)
+     }
+
+     override fun onBindViewHolder(holder: MainAdapter.MainViewHolder, position: Int) {
+
+     }
+
+     override fun getItemCount(): Int {
+         return 5
+     }
+
+     private inner class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+     }
+
+ }
 }
