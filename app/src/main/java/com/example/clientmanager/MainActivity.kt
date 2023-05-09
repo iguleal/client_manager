be6240bb446d562866020f163226001a4b921ce0
Igor Leal <igorleal96@gmail.com>
2023-05-09 12:32:23 -0300

    recycle view, items e icones criados
    recycle view finalizada em grid, icones e items da recycle finalizados

--- app/src/main/java/com/example/clientmanager/MainActivity.kt
+++ app/src/main/java/com/example/clientmanager/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,27 +16,69 @@ class MainActivity : AppCompatActivity() {
 
         val rvMain: RecyclerView = findViewById(R.id.rv_main)
 
-        val adapter = MainAdapter()
+        val listItem = mutableListOf<MainItem>()
+        listItemAdd(listItem)
+
+        val adapter = MainAdapter(listItem)
         rvMain.adapter = adapter
         rvMain.layoutManager = GridLayoutManager(this, 2)
+
+
     }
 
- private inner class MainAdapter(): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
+    private fun listItemAdd(list: MutableList<MainItem>) {
+        list.add(
+            MainItem(
+                id = 1, title = "CLIENTES", img = R.drawable.ic_clients
+            )
+        )
+        list.add(
+            MainItem(
+                id = 2, title = "SERVIÇOS", img = R.drawable.ic_services
+            )
+        )
+        list.add(
+            MainItem(
+                id = 3, title = "RESOLVIDO", img = R.drawable.ic_resolved
+            )
+        )
+        list.add(
+            MainItem(
+                id = 4, title = "À PAGAR", img = R.drawable.ic_payoff
+            )
+        )
+        list.add(
+            MainItem(
+                id = 5, title = "FATURADO", img = R.drawable.ic_billed
+            )
+        )
+    }
+
+    private inner class MainAdapter(
+        val list: MutableList<MainItem>
+    ): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.MainViewHolder {
          val view = layoutInflater.inflate(R.layout.main_item, parent, false)
          return MainViewHolder(view)
      }
 
      override fun onBindViewHolder(holder: MainAdapter.MainViewHolder, position: Int) {
-
+         val itemCurrent = list[position]
+         holder.bind(itemCurrent)
      }
 
      override fun getItemCount(): Int {
-         return 5
+         return list.size
      }
 
      private inner class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+         fun bind(itemCurrent: MainItem){
+             val img: ImageView = itemView.findViewById(R.id.iv_icon_img)
+             val name: TextView = itemView.findViewById(R.id.txt_icon_name)
 
+             img.setImageResource(itemCurrent.img)
+             name.text = itemCurrent.title
+         }
      }
 
  }
---
+++ app/src/main/java/com/example/clientmanager/MainItem.kt
@@ -0,0 +1,7 @@
+package com.example.clientmanager
+
+data class MainItem(
+    val id: Int,
+    val title: String,
+    val img: Int
+)
