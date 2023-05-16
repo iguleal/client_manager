570288ff0d802674d7d574744071225f6caf3b13
Igor Leal <igorleal96@gmail.com>
2023-05-16 13:33:05 -0300

    Adicionado uma tela separa de pagamento
    simplificando a tela de cadastro de cliente, foi adicionada uma tela separada apenas para o pagamento

--- app/src/main/java/com/example/clientmanager/ClientActivity.kt
+++ app/src/main/java/com/example/clientmanager/ClientActivity.kt
@@ -1,12 +1,24 @@
 package com.example.clientmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ClientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client)
-        supportActionBar?.hide()
+
+        val btnPayment: Button = findViewById(R.id.btn_payment)
+
+        btnPayment.setOnClickListener {
+            startActivity(Intent(this, PaymentActivity::class.java))
+        }
+
+        val btnConclude: Button = findViewById(R.id.btn_conclude)
+        btnConclude.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/clientmanager/PaymentActivity.kt
+++ app/src/main/java/com/example/clientmanager/PaymentActivity.kt
@@ -1,11 +1,35 @@
 package com.example.clientmanager
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 
 class PaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        val autoCard: AutoCompleteTextView = findViewById(R.id.auto_card)
+
+        val items = resources.getStringArray(R.array.auto_card)
+        autoCard.setText(items.first())
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        autoCard.setAdapter(adapter)
+
+        autoCard.setOnClickListener {
+            val keyboardService =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            keyboardService.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+
+        val btnFinish: Button = findViewById(R.id.btn_finish)
+        btnFinish.setOnClickListener {
+            finish()
+        }
     }
 }
